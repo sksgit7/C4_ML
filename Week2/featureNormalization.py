8b79d918b3a414f701c53824de5c5907f7f951cd
sksgit7 <56225983+sksgit7@users.noreply.github.com>
2020-04-20 13:30:54 +0530

    Update featureNormalization.py

--- Week2/featureNormalization.py
+++ Week2/featureNormalization.py
@@ -1,13 +1,12 @@
 import numpy as np
-def computeCost(X,y,theta):
+def featureNormalization(X):
     """
-    Take in a numpy array X,y, theta and generate the cost function using theta as parameter in a linear regression model
+    Take in numpy array of X values and return normalize X values,
+    the mean and standard deviation of each feature
     """
-    # np.dot(A,B) => gives dot product of A and B
-    # np.power(A,n) => returns array with each element raised to the power n
-    # np.sum(A) => Returns scalar with every element in A summed up
-    m=len(y)
-    predictions= np.dot(X,theta)
-    square_err= np.power(predictions-y,2)
+    mean=np.mean(X)
+    std=np.std(X)
     
-    return 1/(2*m) * np.sum(square_err)
+    X_norm = (X - mean)/std
+    
+    return X_norm
